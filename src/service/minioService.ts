e03daac9cebc72ab57f083c735578f9e9e36d1cb
Joao Victor <joaocarvoli.eng@alu.ufc.br>
2024-04-08 00:55:55 -0300

    feat: minio tests

--- src/service/minioService.ts
+++ src/service/minioService.ts
@@ -4,9 +4,10 @@ import 'dotenv/config'
 const minioClient = new Minio.Client({
     endPoint: process.env.MINIO_SERVICE_ADDRESS ?? '',
     port: Number(process.env.MINIO_SERVICE_PORT) ?? 0,
-    useSSL: true,
+    useSSL: false,
     accessKey: process.env.MINIO_ACCESS_KEY ?? '',
     secretKey: process.env.MINIO_SECRET_KEY ?? '',
+
 })
 
 const bucket = 'joao'
